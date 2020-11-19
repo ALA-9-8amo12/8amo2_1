32cd435bbff01a2836a9c1649752ddf84f2f1e39
GulerTolga <tolgaguler22@gmail.com>
2020-11-19 19:10:30 +0100

    Added functionalities
    Change between different acticities with the buttons

--- app/src/main/java/com/example/amazighquiz/MainActivity.java
+++ app/src/main/java/com/example/amazighquiz/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.amazighquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void configureOefenActivity(View view) {
+        startActivity(new Intent(MainActivity.this, Oefen.class));
+    }
+
+    public void configureSpeelActivity(View view) {
+        startActivity(new Intent(MainActivity.this, Oefen.class));
+    }
+
+    public void configureScoreActivity(View view) {
+        startActivity(new Intent(MainActivity.this, Oefen.class));
+    }
+
+    public void configureOverActivity(View view) {
+        startActivity(new Intent(MainActivity.this, Oefen.class));
     }
 }
---
+++ app/src/main/java/com/example/amazighquiz/Oefen.java
@@ -0,0 +1,14 @@
+package com.example.amazighquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Oefen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oefen);
+    }
+}
