6e3cf4b4c33a4516cd4381b201e02cc7d3549d70
HolwerdaIvan8amo2 <1041663@mborijnland.nl>
2020-11-23 09:00:24 +0100

    splash screen met delay

---
+++ app/src/main/java/com/example/amazighquiz/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.amazighquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        getSupportActionBar().hide();
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        }, 2000);
+
+
+    }
+}
